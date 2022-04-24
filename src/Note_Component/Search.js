72162c394ca59439e012c9e39fa4417d879cd399
Darab <darabcode@gmail.com>
2022-04-24 21:21:46 +0100

    Removed Search component FontAwesome element

--- src/Note_Component/Search.js
+++ src/Note_Component/Search.js
@@ -1,21 +1,15 @@
 import React from "react";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import "./Search.css";
 
-library.add(faSearch);
 const Search = ({ handleSearch }) => {
   return (
     <div className="search">
-      <input type="text" onChange={(ev) => handleSearch(ev.target.value)} />
-      <span>
-        <FontAwesomeIcon
-          className="font"
-          icon={faSearch}
-          style={{ fontWeight: "100" }}
-        />
-      </span>
+      <input
+        type="text"
+        onChange={(ev) => handleSearch(ev.target.value)}
+        placeholder="Search for your notes..."
+      />
     </div>
   );
 };
